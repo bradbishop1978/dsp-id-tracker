9eaadd05aae4b9658083b8aed4485f1a868e12a5
Brad Penoliar <bernard.penoliar@luladelivery.com>
2025-04-23 09:04:19 -0400

    Update dspid.py

--- dspid.py
+++ dspid.py
@@ -20,7 +20,7 @@ def load_data():
         # Convert empty strings to NaN
         df = df.replace('', pd.NA)
         # Replace "missing" or NaN with ***
-        df = df.replace("missing", "***").fillna("***")
+        df = df.replace("Missing", "***").fillna("***")
         return df
     except Exception as e:
         st.error(f"Error loading data: {e}")
